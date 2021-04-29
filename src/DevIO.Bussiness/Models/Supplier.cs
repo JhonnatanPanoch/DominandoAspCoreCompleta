02a87a0afd149625183d990bb2e65701e9f0c69f
JhonnatanPanoch <43388041+JhonnatanPanoch@users.noreply.github.com>
2021-04-28 23:21:06 -0300

    Criação estrutura inicial + modelos

---
+++ src/DevIO.Bussiness/Models/Supplier.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace DevIO.Bussiness.Models
+{
+    public class Supplier : Entity
+    {
+        public string Name { get; set; }
+        public string Document { get; set; }
+        public SupplierType SupplierType { get; set; }
+        public Address Address { get; set; }
+        public bool Active { get; set; }
+        public IEnumerable<Products> Products { get; set; }
+    }
+}
---
+++ src/DevIO.Bussiness/Models/SupplierType.cs
@@ -0,0 +1,8 @@
+﻿namespace DevIO.Bussiness.Models
+{
+    public enum SupplierType
+    {
+        PHYSICAL = 1,
+        LEGAL
+    }
+}
