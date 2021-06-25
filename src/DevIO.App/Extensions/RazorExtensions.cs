a8bc9230cdc74f8d67129721f3c531f06c9c40d2
JhonnatanPanoch <jpanoch@iurtsbrcsp.onmicrosoft.com>
2021-06-25 14:00:05 -0300

    Finalizando o ajuste das Views
    *RazorExtensions*

---
+++ src/DevIO.App/Extensions/RazorExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+
+namespace DevIO.App.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string FormatDocument(this RazorPage page, int type, string doc)
+        {
+            return type == 1 ? Convert.ToUInt64(doc).ToString(@"000\.000\.000\-00") : 
+                Convert.ToUInt64(doc).ToString(@"00\.000\.000\/0000\-00");
+        }
+    }
+}
