8267e2610bdd2392c85789bbf00f81cd588b8f7f
JhonnatanPanoch <jpanoch@iurtsbrcsp.onmicrosoft.com>
2021-06-28 08:52:55 -0300

    Utilizando Ajax para preenchimento do CEP
    *WS de cep*

--- src/DevIO.App/wwwroot/js/site.js
+++ src/DevIO.App/wwwroot/js/site.js
@@ -35,3 +35,55 @@
         })
     }
 }
+
+function bindCepForm() {
+    function cepFormValues(value) {
+        $("#publicPlace").val(value);
+        $("#district").val(value);
+        $("#city").val(value);
+        $("#state").val(value);
+    }
+
+    $("#cep").blur(function () {
+
+        var cep = $(this).val().replace(/\D/g, '');
+
+        if (cep != "") {
+            var cepValidation = /^([\d]{2})\.*([\d]{3})-*([\d]{3})/
+
+            if (cepValidation.test(cep)) {
+                cepFormValues("...");
+
+                $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (data) {
+                    if (!("erro" in data)) {
+
+                        $("#publicPlace").val(data.logradouro);
+                        $("#district").val(data.bairro);
+                        $("#city").val(data.localidade);
+                        $("#state").val(data.uf);
+                    } else {
+                        cepFormValues("");
+                        alert("CEP não encontrado.");
+                    }
+                });
+            } else {
+                cepFormValues("");
+                alert("Formato de CEP inválido.");
+            }
+        } else {
+            cepFormValues("");
+        }
+    });
+}
+
+function formatCEP(str) {
+    var re = /^([\d]{2})\.*([\d]{3})-*([\d]{3})/; // Pode usar ? no lugar do *
+
+    if (re.test(str)) {
+        return str.replace(re, "$1.$2-$3");
+    } else {
+        alert("CEP inválido!");
+    }
+
+    return "";
+}
